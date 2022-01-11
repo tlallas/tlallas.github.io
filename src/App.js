2c5d4c1f809f712a4a077d4ba4ead2a36c998ca0
Taylor Lallas <tlallas@stanford.edu>
2022-01-10 17:06:59 -0800

    updated landing jumbotron

--- src/App.js
+++ src/App.js
@@ -19,30 +19,36 @@ function App() {
         </a>
         <div className="App-navbar-items">
           <a href="#about">about</a>
+          <a href="#projects">projects</a>
           <a href="#resume">resume</a>
-          <a href="#">portfolio</a>
-          <a href="#">contact</a>
+          <a href="#contact">contact</a>
         </div>
       </div>
       <header className="Landing-jumbotron">
         <img src={profile}  alt="logo" className="profile-photo"/>
         <div className="Landing-jumbo-text-container"> 
-          <h1 style={{fontSize: "3em"}}>hi, </h1>
-          <h1 style={{}}> i'm <span style={{color: "rgb(179, 6, 6)"}}>taylor lallas</span>! </h1>
-          <p>frontend engineer,</p>
-          <p>ui / ux designer,</p>
-          <p>biz strategist.</p>
-        </div>
+          <h1 style={{fontSize: "3em", marginBottom: '-1%'}}>hi, </h1>
+          <h1 style={{marginBottom: '-3%'}}> i'm <span style={{color: "rgb(179, 6, 6)"}}>taylor lallas</span>! </h1>
+          <p>product builder<b style={{fontSize: "1.5em"}}>.</b>  ui/ux designer<b style={{fontSize: "1.5em"}}>.</b>   biz strategist<b style={{fontSize: "1.5em"}}>.</b> </p>
+          <div className="recent-highlights">
+            <p className="mini-label">now <br/> <span className="recent-point">Teaching <a target="_blank" href="https://hci.stanford.edu/courses/cs147/2022/wi/index.html" className="recent-point-resource">Human-Centered Design</a> @ Stanford<br/>Pursuing a Master's in Computer Science @ Stanford</span></p>
+            <p className="mini-label">previously <br/> <span className="recent-point">Emerging Products @ T-Mobile
+             <br/>New Product Experimentation & Novi  @ Meta</span>
+             <br/>
+             </p>
+
+          </div>
+   </div>
       </header>
       <div id="about" className="aboutSection">
           <h2 className="aboutTitle">about</h2>
           <div className="aboutText">
             I am a <span className="highlightText">Master's</span> student in <span className="highlightText">Computer Science</span> at <span className="highlightText">Stanford University</span>. Though I have broad exposure across
-            CS disciplines including systems, security, and algorithmic theory, I am focused on <span className="highlightText">Human-Computer Interaction</span>. 
+            CS disciplines including systems, security, and algorithmic theory, I am focused on <span className="highlightText">Human-Computer Interaction Design</span>. 
 
             <br/>
             <br/>
-            I also earned my Bachelor's degree in Economics from Stanford ('21). During my undergraduate time at The Farm, 
+            I also earned my <b className="highlightText">Bachelor's degree in Economics</b> from Stanford ('21). During my undergraduate time at The Farm, 
             I was a member of the Stanford Women's Tennis Team. We captured two NCAA national championships in those four years, and I had the honor of co-captaining the 
             squad for my senior season.
             </div>
